468ee99f806ebaf004213f718e6893ed94812046
bilal <me@example.com>
2022-03-19 01:09:03 +0500

    reset

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,40 +1,13 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
-import { BrowserRouter} from "react-router-dom";
-import MyNavbar from "./components/MyNavbar";
-import { useState } from "react";
-import Operations from "./components/Operations";
-import MyTable from "./components/MyTable";
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
+
+
 
 function App() {
-
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount(count+1)
-  };
-
-  const decrement = () => {
-    setCount(count-1)
-  };
-
-
 
 return (
 
   <>
-
-<div>
-    <MyNavbar state={count}/>
-    <BrowserRouter>
-    <Routes>
-    <Route exact path="home" element={<Home/>} />
-    <Route exact path="table" element={<MyTable/>} />
-    <Route exact path="operations"  element={<Operations increment={increment} decrement={decrement}/>}/>
-    </Routes>
-    </BrowserRouter>
-</div>
 
  
   </>
