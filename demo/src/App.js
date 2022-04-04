dcbf76c453cd2d2d42d56be47bd51b2a9dc0fcac
bilal <me@example.com>
2022-04-04 23:16:46 +0500

    useReducer

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,18 +1,45 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
-
-
+import { useState, useReducer } from "react";
 
 function App() {
-
-return (
-
-  <>
-
- 
-  </>
-  
-)
-  
+  // USE STATE HOOK
+  // const [data, setData] = useState({
+  //   name: "",
+  //   email: "",
+  // });
+  // const handleChange = (e) => {
+  //   let name = e.target.name;
+  //   let value = e.target.value;
+  //   setData({
+  //     ...data,
+  //     [name]: value,
+  //   });
+  // };
+  // return (
+  //   <>
+  //     <input name="name" onChange={handleChange} />
+  //     <input name="email" onChange={handleChange} />
+  //   </>
+  // );
+  //USE REDUCER HOOK
+  // const reducer = (state, action) => {
+  //   switch (action.type) {
+  //     case "CHANGENAME":
+  //       return {
+  //         name: action.name,
+  //       };
+  //   }
+  // };
+  // const [state, dispatch] = useReducer(reducer, { name: "" });
+  // return (
+  //   <>
+  //     <input
+  //       onChange={(e) => {
+  //         dispatch({ type: "CHANGENAME", name: e.target.value });
+  //       }}
+  //     />
+  //     <h1>{JSON.stringify(state)}</h1>
+  //   </>
+  // );
 }
 export default App;
