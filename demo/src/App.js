a08dd9ae493cf4b5c8d7fd6402b7face199877f7
bilal <me@example.com>
2022-04-05 05:11:30 +0500

    done

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,18 +1,27 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
-
-
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useParams,
+  useSearchParams,
+} from "react-router-dom";
+import { useRef } from "react";
 
 function App() {
-
-return (
-
-  <>
-
- 
-  </>
-  
-)
-  
+  const inputRef = useRef(null);
+  const [searchParams, setSearchParams] = useSearchParams();
+  return (
+    <>
+      <input ref={inputRef} />
+      <button
+        onClick={() => {
+          setSearchParams({ filter: inputRef.current.value });
+        }}
+      >
+        set data
+      </button>
+    </>
+  );
 }
 export default App;
--- demo/src/index.js
+++ demo/src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useParams,
+  useSearchParams,
+} from "react-router-dom";
 
 ReactDOM.render(
-<App/>, document.getElementById('root'));
-
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<App />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
