3f2bb713298f4fbb2b2e833b05b54d58edf216f9
bilal <me@example.com>
2022-05-08 21:25:41 +0500

    simple practice

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,18 +1,42 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
-
-
+import { useEffect, useState } from "react";
 
 function App() {
+  // const obj = {
+  //   text: "Hello",
+  // };
 
-return (
+  // const obj1 = {
+  //   text: "Hello",
+  // };
+  const [counter, setCounter] = useState(0);
+  // const getData = () => {
+  //   console.log("hello");
+  //   return 1 + 2 + 3;
+  // };
+  const [state, setState] = useState();
 
-  <>
+  useEffect(() => {
+    // setCounter((counter) => {
+    //   return counter + 1;
+    // });
+    // setCounter((counter) => {
+    //   return counter + 1;
+    // });
+    //console.log("iam inside useeffect");
+  }, []);
+  const handleIncrement = () => {
+    setCounter((counter) => {
+      return counter + 1;
+    });
+  };
 
- 
-  </>
-  
-)
-  
+  return (
+    <>
+      <h1>{counter}</h1>
+      <button onClick={handleIncrement}> increment</button>
+    </>
+  );
 }
 export default App;
