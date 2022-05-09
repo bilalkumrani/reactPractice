1f9ec19070ef4ed2bac623ce6ad4aa58b1cd0fa1
bilal <me@example.com>
2022-05-09 12:42:31 +0500

    add cake restocked

--- redux_demo/index.js
+++ redux_demo/index.js
@@ -1,88 +1,41 @@
-// const redux = require("redux");
-// const creteStore = redux.createStore;
-// //Action
-// const CAKE_ORDERED = "CAKE_ORDERED";
-
-// const orderCake = (quantity) => {
-//   return {
-//     type: CAKE_ORDERED,
-//     quantity,
-//   };
-// };
-
-// //Reducer (previous state, action) => newState
-
-// const initialState = {
-//   numOfCakes: 10,
-// };
-
-// const reducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case CAKE_ORDERED:
-//       return {
-//         ...state,
-//         numOfCakes: state.numOfCakes - action.quantity,
-//       };
-
-//     default:
-//       return state;
-//   }
-// };
-
-// // Rdux store
-// // holds application state, Allow access to state via getState(),
-// // Allow state to be updated via dispatch(action)
-// // register listeners via subscribe(listener)
-// // handle unregistering of listeners via the function returned by subscribe(listener)
-
-// const store = creteStore(reducer);
-// console.log("this is state: ", store.getState());
-// const unsubscribe = store.subscribe(() => {
-//   console.log(store.getState());
-// });
-
-// store.dispatch(orderCake(2));
-// store.dispatch(orderCake(1));
-// store.dispatch(orderCake(0));
-
-// unsubscribe();
-
 const redux = require("redux");
-const createStore = redux.createStore;
-// Actions
+const creteStore = redux.createStore;
+//Action
+const CAKE_ORDERED = "CAKE_ORDERED";
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
 
-const INCREMENT = "INCREMENT";
-const DECREMENT = "DECREMENT";
-
-const increment = (quantity) => {
+const cakeRestocked = (quantity = 1) => {
   return {
-    type: INCREMENT,
-    quantity,
+    type: CAKE_RESTOCKED,
+    payload: quantity,
   };
 };
-const decrement = (quantity) => {
+
+const orderCake = (quantity = 1) => {
   return {
-    type: DECREMENT,
-    quantity,
+    type: CAKE_ORDERED,
+    payload: quantity,
   };
 };
-// reducer
+
+//Reducer (previous state, action) => newState
 
 const initialState = {
-  counter: 0,
+  numOfCakes: 10,
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case INCREMENT:
+    case CAKE_ORDERED:
       return {
         ...state,
-        counter: state.counter + action.quantity,
+        numOfCakes: state.numOfCakes - action.payload,
       };
-    case DECREMENT:
+
+    case CAKE_RESTOCKED:
       return {
         ...state,
-        counter: state.counter - action.quantity,
+        numOfCakes: action.payload,
       };
 
     default:
@@ -90,14 +43,77 @@ const reducer = (state = initialState, action) => {
   }
 };
 
-// Store
+// Rdux store
+// holds application state, Allow access to state via getState(),
+// Allow state to be updated via dispatch(action)
+// register listeners via subscribe(listener)
+// handle unregistering of listeners via the function returned by subscribe(listener)
 
-const store = createStore(reducer);
-const unsubscribe = store.subscribe(() => console.log("store is subscribed"));
+const store = creteStore(reducer);
+console.log("this is state: ", store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
 
-store.dispatch(increment(4));
-console.log(store.getState());
+store.dispatch(orderCake(2));
+store.dispatch(orderCake(1));
+store.dispatch(orderCake(0));
 
-store.dispatch(decrement(2));
+store.dispatch(cakeRestocked(10));
 
-console.log(store.getState());
+unsubscribe();
+
+// const redux = require("redux");
+// const createStore = redux.createStore;
+// // Actions
+
+// const INCREMENT = "INCREMENT";
+// const DECREMENT = "DECREMENT";
+
+// const increment = (quantity) => {
+//   return {
+//     type: INCREMENT,
+//     quantity,
+//   };
+// };
+// const decrement = (quantity) => {
+//   return {
+//     type: DECREMENT,
+//     quantity,
+//   };
+// };
+// // reducer
+
+// const initialState = {
+//   counter: 0,
+// };
+
+// const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case INCREMENT:
+//       return {
+//         ...state,
+//         counter: state.counter + action.quantity,
+//       };
+//     case DECREMENT:
+//       return {
+//         ...state,
+//         counter: state.counter - action.quantity,
+//       };
+
+//     default:
+//       return state;
+//   }
+// };
+
+// // Store
+
+// const store = createStore(reducer);
+// const unsubscribe = store.subscribe(() => console.log("store is subscribed"));
+
+// store.dispatch(increment(4));
+// console.log(store.getState());
+
+// store.dispatch(decrement(2));
+
+// console.log(store.getState());
